a98b320d6b1312480787ff88182b8099ebc2286f
sofiesty <sofie04@gmail.com>
2025-10-24 08:30:12 +0200

    fikset style

--- quiz.js
+++ quiz.js
@@ -1,19 +1,13 @@
-// Functions
 function buildQuiz(){
-    // variable to store the HTML output
     const output = [];
   
-    // for each question...
     myQuestions.forEach(
       (currentQuestion, questionNumber) => {
   
-        // variable to store the list of possible answers
         const answers = [];
   
-        // and for each available answer...
         for(letter in currentQuestion.answers){
   
-          // ...add an HTML radio button
           answers.push(
             `<label>
               <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -23,7 +17,6 @@ function buildQuiz(){
           );
         }
   
-        // add this question and its answers to the output
         output.push(
           `<div class="question"> ${currentQuestion.question} </div>
           <div class="answers"> ${answers.join('')} </div>`
@@ -31,49 +24,41 @@ function buildQuiz(){
       }
     );
   
-    // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
   }
 
   function showResults(){
 
-    // gather answer containers from our quiz
     const answerContainers = quizContainer.querySelectorAll('.answers');
   
-    // keep track of user's answers
     let numCorrect = 0;
   
-    // for each question...
     myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-      // find selected answer
+ 
       const answerContainer = answerContainers[questionNumber];
       const selector = `input[name=question${questionNumber}]:checked`;
       const userAnswer = (answerContainer.querySelector(selector) || {}).value; 
   
-      // if answer is correct
       if(userAnswer === currentQuestion.correctAnswer){
-        // add to the number of correct answers
+        
         numCorrect++;
-  
-        // color the answers green
-        answerContainers[questionNumber].style.color = 'lightgreen';
+   
+        answerContainers[questionNumber].style.color = 'green';
       }
-      // if answer is wrong or blank
+     
       else{
-        // color the answers red
-        answerContainers[questionNumber].style.color = 'red';
+        
+        answerContainers[questionNumber].style.color = 'darkred';
       }
     });
   
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    resultsContainer.innerHTML = `${numCorrect} av ${myQuestions.length} riktige`;
   }
 
-// Variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+
 const myQuestions = [
     {
       question: "Who invented JavaScript?",
@@ -105,10 +90,8 @@ const myQuestions = [
       }
     ];
 
-// Kick things off
 buildQuiz();
 
-// Event listeners
 submitButton.addEventListener('click', showResults);
 
 
